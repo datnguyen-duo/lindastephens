50284fbddccbc0042e02a17782f1d735c64cc23a
Dat Nguyen <dat@duo-studio.co>
2023-09-22 15:22:51 -0400

    qa

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function () {
       document.querySelector(".about__container").classList.remove("active");
     });
+
+  var serviceToggles = document.querySelectorAll("#sec__004 ul li"),
+    serviceBox = document.querySelectorAll("#sec__004 .service-box");
+
+  serviceToggles.forEach((toggle, i) => {
+    toggle.addEventListener("click", function () {
+      serviceToggles.forEach((toggleClass) => {
+        toggleClass.classList.remove("active");
+      });
+
+      toggle.classList.add("active");
+      serviceBox.forEach((box) => {
+        box.classList.remove("active");
+      });
+      serviceBox[i].classList.add("active");
+    });
+  });
 });
